c2d9f1c726879bf6360f0e6b6c3e845a1b05d3c8
DaMbY1 <dmitrybab80@mail.ru>
2025-07-31 21:12:44 +0300

    Update markups.py
    Removed spaces

--- keyboards/default/markups.py
+++ keyboards/default/markups.py
@@ -9,23 +9,19 @@ def confirm_markup():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add(confirm_message)
     markup.add(back_message)
-
     return markup
 
 def back_markup():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add(back_message)
-
     return markup
 
 def check_markup():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.row(back_message, all_right_message)
-
     return markup
 
 def submit_markup():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.row(cancel_message, all_right_message)
-
     return markup
