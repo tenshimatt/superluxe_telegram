274c16aa66f7eb727c372ff42b2c8a2dfb23f7ae
root <root@opencode.beyondpandora.com>
2025-11-29 00:06:14 +0000

    Fix circular import issues in keyboard modules
    - Move database imports to be lazy (inside functions)
    - Fix InlineKeyboardButton parameter issues
    - Prevent bot initialization during module import

--- keyboards/inline/products_from_catalog.py
+++ keyboards/inline/products_from_catalog.py
@@ -1,6 +1,5 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.callback_data import CallbackData
-from loader import db
 
 product_cb = CallbackData("product", "id", "action")
 
